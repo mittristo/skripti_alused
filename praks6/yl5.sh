737f4c9f7b942a9ce77263a32ed390414dd022f7
Risto Mitt <mitt.risto@khk.ee>
2020-11-19 16:44:11 +0200

    t2rnid rivis

---
+++ praks6/yl5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##Väljastab 5x5 tärni reanimega
+for (( i = 1; i <= 5; i++ ))      ### Outer for loop ###
+do
+echo -n "$i. "
+	for ((j=1; j<=5; j++)); do
+          echo -n "x "
+    done
+  echo "" #### print the new line ###
+done
+
