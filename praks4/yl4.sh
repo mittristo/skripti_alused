04531922526f30cb7a3869ec3f10dea00b36414e
Risto Mitt <mitt.risto@khk.ee>
2020-11-19 12:13:05 +0200

    mis arv anti

--- praks4/yl4.sh
+++ praks4/yl4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ##Kas arv on paaris või paaritu
-echo "Sisesta suvaline täisarv: "
+
 j22k=$(( $1 % 2 ))
 if [ $j22k -eq 0 ]
 then
-	echo $arv " on paarisarv"
+	echo " $1" " on paarisarv"
 else
-	echo $arv " on paaritu arv"
+	echo "$1" " on paaritu arv"
 fi
