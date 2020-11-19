c59c28ceae783ac2cbc51894297cf545a43f69a6
Risto Mitt <mitt.risto@khk.ee>
2020-11-19 14:12:25 +0200

    Kellaaja automaatne lisamine

---
+++ praks5/yl4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#kellaajajärgi tervitamine
+kasutaja=`whoami`
+kell=`date +%H`
+#teeme if-i
+if [ $kell -ge 6 -a $kell -lt 12 ]; then
+        echo "Tere hommikust!" $kasutaja
+elif [ $kell -ge 12 -a $kell -lt 18 ]; then
+        echo "Tere p2evast!" $kasutaja
+elif [ $kell -ge 18 -a $kell -lt 22 ]; then
+        echo "Tere 6htust" $kasutaja
+elif [ $kell -ge 22 -a $kell -lt 24  ] || [ $kell -ge 0 -a $kell -lt 6 ]; then
+        echo "Head 66d!" $kasutaja
+
+else
+        echo "Tundmatu kellaaeg"
+fi
+
