f88b098a06b359f8401ea47786eb03404d40cfcc
Risto Mitt <mitt.risto@khk.ee>
2020-11-19 11:55:25 +0200

    tostsin ymber yl3.sh

---
+++ praks2/yl3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###See on skript kuup2eva kohta
+tunnid=`date +%H`
+min=`date +%M`
+echo  "Kell on praegu "$tunnid" tundi ja "$min " minutit"
+kuu=`date +%B`
+aasta=`date +%Y`
+echo  $kuu", "$aasta
+cal=`cal`
+echo $cal
